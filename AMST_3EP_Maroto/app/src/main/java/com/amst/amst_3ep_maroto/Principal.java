b6b918616ef6603630e1a51e83d1b0d8867bc0df
dmaroto98 <dmaroto@espol.edu.ec>
2021-02-13 16:40:27 -0500

    apk subida y app terminada

--- AMST_3EP_Maroto/app/src/main/java/com/amst/amst_3ep_maroto/Principal.java
+++ AMST_3EP_Maroto/app/src/main/java/com/amst/amst_3ep_maroto/Principal.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,6 +15,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
@@ -22,16 +24,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Principal extends AppCompatActivity {
-    Button btn_id;
     DatabaseReference db_reference;
+    LinearLayout paises_contenedor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
         Intent intent = getIntent();
-        iniciarBaseDeDatos();
-//        leerTweets();
+        paises_contenedor= findViewById(R.id.paises_contenedor);
 
     }
 
@@ -43,38 +44,28 @@ public class Principal extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void iniciarBaseDeDatos(){
-        ArrayList<String> paises = new ArrayList<String>();
-        ArrayList<String> totalCasos = new ArrayList<String>();
+    public void iniciarBaseDeDatos(View view){
+        db_reference = FirebaseDatabase.getInstance().getReference();
 
 
-//        db_reference = FirebaseDatabase.getInstance().getReference().child("0");
-        db_reference = FirebaseDatabase.getInstance().getReference().child("0");
-        for(int i=0; i<=98;i++) {
-            db_reference.child("Country").child("Region").addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                        System.out.println(snapshot.getValue());
-                    }
+        db_reference.orderByChild("TotalCases").limitToLast(5).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    System.out.println(snapshot);
+                    TextView t=new TextView(getApplicationContext());
+                    String re="Pais: "+(String.valueOf(snapshot.child("Country").child("Region").getValue()))+"              Total de Casos: "+(String.valueOf(snapshot.child("TotalCases").getValue()));
+                    t.setText(re);
+                    paises_contenedor.addView(t);
                 }
+            }
 
-                @Override
-                public void onCancelled(DatabaseError error) {
-                    System.out.println(error.toException());
-                }
-            });
-        }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                System.out.println(error.toException());
+            }
+        });
 
-
-        System.out.println(paises);
-        System.out.println(totalCasos);
     }
-
-
-
-
-
-
 
 }
